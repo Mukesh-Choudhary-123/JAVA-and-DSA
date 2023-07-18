911cc2daff5170d18068a5c8c2acd3bdb1986589
Mukesh Choudhary <118448246+Mukesh-Choudhary-123@users.noreply.github.com>
2023-07-18 22:15:29 +0530

    Me
    java basics print star pattern

---
+++ printpattern/print_pattern.java
@@ -0,0 +1,10 @@
+package Alpha.printpattern;
+
+public class print_pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("*\t*");
+        System.out.println("*\t*\t*");
+        System.out.println("*\t*\t*\t*");
+    }
+}
