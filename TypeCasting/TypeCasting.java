9a3f12f6ab938607324c5457e57503f6d295f796
Mukesh Choudhary <118448246+Mukesh-Choudhary-123@users.noreply.github.com>
2023-07-19 11:58:30 +0530

    7. float to int & char to int

---
+++ TypeCasting/TypeCasting.java
@@ -0,0 +1,17 @@
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        // Type Casting float to int
+        float a = 25.567f;
+        int b = (int) a;
+        System.out.println("value of a(float) befor type casting = " + a);
+        System.out.println("value of b(int) after type casting = " + b);
+
+        // Type Casting char to int
+        char ch = 'a';
+        int num = (int) ch;
+        System.out.println("value of ch(char) befor type casting = " + ch);
+        System.out.println("value of num(int) after type casting = " + num);
+    }
+}
