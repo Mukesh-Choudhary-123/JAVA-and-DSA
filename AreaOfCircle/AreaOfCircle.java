513874ead7df27faba8c493d346c61ab7cf27652
Mukesh Choudhary <118448246+Mukesh-Choudhary-123@users.noreply.github.com>
2023-07-19 11:34:45 +0530

    6. Area of circle

---
+++ AreaOfCircle/AreaOfCircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        float pi = 3.14f;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius = ");
+        float radius = sc.nextFloat();
+        float area = pi * radius * radius;
+        System.out.println("Area of circle is = " + area);
+    }
+}
