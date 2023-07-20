4f58b1d2719dcccb667fcf63a6510ecdd95d718a
Mukesh Choudhary <118448246+Mukesh-Choudhary-123@users.noreply.github.com>
2023-07-20 16:54:03 +0530

    10. Switch (Conditional Statement )

---
+++ Calculater/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b : ");
+        int b = sc.nextInt();
+        System.out.print("Enter the opreator : ");
+        char op = sc.next().charAt(0);
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("Invalid Opreator");
+                break;
+        }
+    }
+}
